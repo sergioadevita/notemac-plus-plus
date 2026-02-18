dc866e0e71b3ec15f73e613a9f543222ec9818ae
Sergio A. De Vita <sergio.a.devita@gmail.com>
2026-02-18 21:07:21 +0000

    v2.2.2: type safety audit, code dedup, constants extraction, critical bug fixes

--- src/components/FeedbackPopup.tsx
+++ src/components/FeedbackPopup.tsx
@@ -8,7 +8,7 @@ interface FeedbackPopupProps
 
 const FEEDBACK_TIMER_MS = 20 * 60 * 1000;
 const FEEDBACK_STORAGE_KEY = 'notemac_feedback_shown_version';
-const APP_VERSION = '2.2.1';
+const APP_VERSION = '2.2.2';
 
 export function FeedbackPopup({ theme }: FeedbackPopupProps)
 {
