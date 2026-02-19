0a774f4e6d2eccfc8a6819910b2e225820a6e153
Sergio A. De Vita <sergio.a.devita@gmail.com>
2026-02-19 07:54:58 +0000

    Update src/components/GoToLineDialog.tsx

--- src/components/GoToLineDialog.tsx
+++ src/components/GoToLineDialog.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { useEditorStore } from '../store/editorStore';
 import type { ThemeColors } from '../utils/themes';
 
